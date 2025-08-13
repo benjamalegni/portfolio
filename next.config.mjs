2bb251eff1de3dabe125816e533297935e7af3bc
benjamalegni <lukabenjaminmalegni@gmail.com>
2025-08-13 09:05:58 -0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isGhPages = true
-const repoName = 'portfolio'
-
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
   output: 'export',
   basePath: '/portfolio',
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 }
 
 export default nextConfig
